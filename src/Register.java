dba57d3ceed9f399467b2593bb1cdbb0fd8e4c3c
Kevin27182 <kevinmnard@gmail.com>
2024-08-30 18:06:09 -0500

    Added empty Denomination record

--- src/Register.java
+++ src/Register.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Register {
+
+    public Purse makeChange(double amt) {
+        return new Purse();
+    }
+
+    public static void main(String[] args) {
+        Register example_register = new Register();
+        System.out.println(example_register.makeChange(0.0));
+    }
 }
